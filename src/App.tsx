ef8b9ab778721a73a75503d5c9c4e8ea4023e47b
Anton Kozyriev <sonara4mus@gmail.com>
2022-05-26 01:05:29 +0300

    fix: set application structure to flexbox instead of gridview

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 function App() {
   return (
-    <div className="application w-screen h-screen">
-      <div className="drafts"></div>
-      <div className="workspace"></div>
-      <div className="toolbar"></div>
+    <div className="flex flex-row w-screen h-screen">
+      <div className="grow">
+        <div className="flex flex-col w-full h-full">
+          <div className="basis-12 bg-slate-300">
+            Toolbar
+          </div>
+          <div className="grow bg-slate-900">
+            Workspace
+          </div>
+        </div>
+      </div>
+      <div className="hidden sm:block basis-64 bg-slate-600">
+        Drafts
+      </div>
     </div>
   );
 }
