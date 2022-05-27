05ff58237186b55dac6d139d39eecec6d77ce8f8
Anton Kozyriev <sonara4mus@gmail.com>
2022-05-27 21:30:40 +0300

    feat: style containers for application structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 function App() {
   return (
-    <div className="flex flex-row w-screen h-screen">
+    <div className="bg-slate-100/50 flex flex-row gap-2 p-2 w-screen h-screen">
       <div className="grow">
-        <div className="flex flex-col w-full h-full">
-          <div className="basis-12 bg-slate-300">
-            Toolbar
+        <div className="flex flex-col gap-2 w-full h-full">
+          <div className="basis-12 container">
           </div>
-          <div className="grow bg-slate-900">
-            Workspace
+          <div className="grow container">
           </div>
         </div>
       </div>
-      <div className="hidden sm:block basis-64 bg-slate-600">
-        Drafts
+      <div className="hidden sm:block basis-64 container">
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: colors.transparent,
+      slate: colors.slate,
+      neutral: {
+        DEFAULT: "#3D4451",
+        focus: "#303640",
+        content: "#FFFFFF"
+      },
+      base: {
+        100: "#FFFFFF",
+        200: "#F2F2F2",
+        300: "#E5E6E6",
+        content: "#1F2937"
+      }
+    }
   },
   plugins: [],
 }
