592139a25c62d59e36035a799edd9ddc5e283eb7
Anton Kozyriev <sonara4mus@gmail.com>
2022-06-03 23:28:11 +0300

    fix: remove leading loose from editing fields

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -25,12 +25,12 @@ function Editor() {
             {isRender ? (
                 <div className="flex flex-col p-4 w-full h-full">
                     <div className="p-2 font-extrabold w-full h-10 text-neutral">{filename}</div>
-                    <div className="bg-transparent px-2 py-1 mt-2 leading-loose w-full h-full" style={renderBlockStyles} dangerouslySetInnerHTML={{ __html: renderedEquations }} ></div>
+                    <div className="bg-transparent px-2 py-1 mt-2 w-full h-full" style={renderBlockStyles} dangerouslySetInnerHTML={{ __html: renderedEquations }} ></div>
                 </div>
             ) : (
                 <div className="flex flex-col p-4 w-full h-full">
                     <FilenameInput />
-                    <textarea className="flex-auto mt-2 rounded-md bg-transparent px-2 py-1 leading-loose w-full h-content border-0 outline-none hover:bg-neutral/10 focus:bg-neutral/10"
+                    <textarea className="flex-auto mt-2 rounded-md bg-transparent px-2 py-1 w-full h-content border-0 outline-none hover:bg-neutral/10 focus:bg-neutral/10"
                         style={textAreaStyles} value={content} onChange={e => dispatch(setContent(e.target.value))}
                         placeholder="Type your text or formulas here. Hold 'ALT' to toggle render mode. To render equations wrap them into $ ... $.">
                     </textarea>
