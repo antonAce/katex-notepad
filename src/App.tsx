84855a085ba12c13acf67a6e33846c3a749e72a5
Anton Kozyriev <sonara4mus@gmail.com>
2022-06-03 22:33:18 +0300

    feat: toggle render mode with key bindings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { contextMenuEvent } from './services/config';
+
+import { toggleRender } from './store/slice/editor';
+import { useAppDispatch } from './store/hooks';
+
 import NavBar from './components/Navbar';
 import Editor from './components/Editor';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    contextMenuEvent();
+
+    document.addEventListener('keydown', (e) => { if (e.key === "Shift") { dispatch(toggleRender(true)); } });
+    document.addEventListener('keyup', (e) => { if (e.key === "Shift") { dispatch(toggleRender(false)); } });
+  });
+
   return (
     <div className="flex flex-col w-screen h-screen">
       <div className="basis-14 min-h-14"><NavBar /></div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 
-import { contextMenuEvent } from './services/config';
 import { store } from './store/config'
 
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +9,7 @@ import App from './App';
 import 'katex/dist/katex.min.css';
 import './index.css';
 
-contextMenuEvent();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<Provider store={store}><App /></Provider>);
+
 reportWebVitals();
