08ae99e5604a2c702def2e6c484212c2600ed57b
Anton Kozyriev <sonara4mus@gmail.com>
2022-06-03 17:58:01 +0300

    feat: record font size into state management

--- src/store/slice/editor.ts
+++ src/store/slice/editor.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface EditorState {
     isRender: boolean;
     content: string;
+    fontSize: number;
 }
 
 export const initialState: EditorState = {
     isRender: false,
-    content: ""
+    content: "",
+    fontSize: 14
 }
 
 export const editorSlice = createSlice({
@@ -15,9 +17,10 @@ export const editorSlice = createSlice({
     initialState,
     reducers: {
         toggleRender: (state, action: PayloadAction<boolean>) => { state.isRender = action.payload; },
-        setContent: (state, action: PayloadAction<string>) => { state.content = action.payload; }
+        setContent: (state, action: PayloadAction<string>) => { state.content = action.payload; },
+        setFontSize: (state, action: PayloadAction<number>) => { state.fontSize = action.payload; }
     }
 })
 
-export const { toggleRender, setContent } = editorSlice.actions
+export const { toggleRender, setContent, setFontSize } = editorSlice.actions
 export default editorSlice.reducer
