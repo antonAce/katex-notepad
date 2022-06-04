5f5d6e43b62ab8cf7b5c27c50b60b2332836fbe3
Anton Kozyriev <sonara4mus@gmail.com>
2022-06-04 20:16:58 +0300

    fix(Button.tsx): add missing dark mode colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 function Button(props: ButtonProps) {
     return (
         <button type="button" onClick={props.onClick} onMouseEnter={props.onMouseEnter} onMouseLeave={props.onMouseLeave}
-            className="w-8 h-8 inline-block p-1 rounded-md text-base-800 dark:text-base-300 leading-normal uppercase bg-transparent focus:bg-base-800/10 hover:bg-base-800/10 active:bg-base-900/50">{props.children}</button>
+            className="w-8 h-8 inline-block p-1 rounded-md text-base-800 dark:text-base-300 leading-normal uppercase bg-transparent focus:bg-base-800/10 hover:bg-base-800/10 active:bg-base-900/50 dark:hover:bg-base-200/10 dark:focus:bg-base-200/10 dark:active:bg-base-100/50">{props.children}</button>
     );
 }
 
