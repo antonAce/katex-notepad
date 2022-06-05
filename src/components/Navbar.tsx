7f17ad5b34b965a32cb2216161d6216c96964831
Anton Kozyriev <sonara4mus@gmail.com>
2022-06-06 00:52:12 +0300

    feat: spinner component for saving project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,6 +16,7 @@ const DarkModeIcon = memo(() => (<svg xmlns="http://www.w3.org/2000/svg" classNa
 const LightModeIcon = memo(() => (<svg xmlns="http://www.w3.org/2000/svg" className="w-icons h-icons" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /></svg>));
 
 function NavBar() {
+    const isSaving = useAppSelector((state: RootState) => state.editor.isSaving);
     const filepath = useAppSelector((state: RootState) => state.editor.filepath);
     const content = useAppSelector((state: RootState) => state.editor.content);
 
@@ -36,9 +37,15 @@ function NavBar() {
                         </svg>
                     </Button>
                     <Button onClick={_ => dispatch(saveContentToFile({ content, filepath }))}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-icons h-icons" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-                        </svg>
+                        {
+                            isSaving ?
+                                (<span className="spinner"></span>) :
+                                (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-icons h-icons" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                                    </svg>
+                                )
+                        }
                     </Button>
                     <Button>
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-icons h-icons" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
