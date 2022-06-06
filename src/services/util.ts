8163753ad18dc3c0520647163c2c49a57499c874
Anton Kozyriev <sonara4mus@gmail.com>
2022-06-07 01:26:09 +0300

    feat: util to replace filename in path

--- src/services/util.ts
+++ src/services/util.ts
@@ -2,3 +2,7 @@ export function stripFilename(filepath: string): string {
     const matches = filepath.match(/^.*[\\/](.*)\..+$/);
     return (matches !== null && matches.length > 1) ? matches[1] : filepath;
 }
+
+export function replaceFilenameInPath(filepath: string, name: string): string {
+    return filepath.replace(/(^.*[\\/]).*(\..+$)/, "$1" + name + "$2");
+}
